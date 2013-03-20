95e895ca847e229e959f94d0a701a623f31a8fe2
Lars Bilke <lars.bilke@ufz.de>
2013-03-20 17:05:42 +0100

    Fixed typo.

--- files/brews/jenkins.rb
+++ files/brews/jenkins.rb
@@ -12,26 +12,4 @@ class Jenkins < Formula
     libexec.install "jenkins.war"
   end
 
-  #plist_options :manual => "java -jar #{HOMEBREW_PREFIX}/opt/jenkins/libexec/jenkins.war"
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>/usr/bin/java</string>
-          <string>-jar</string>
-          <string>#{opt_prefix}/libexec/jenkins.war</string>
-          <string>--httpListenAddress=127.0.0.1</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-    </plist>
-  EOS
-  end
 end
