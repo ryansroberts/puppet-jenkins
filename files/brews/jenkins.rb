849ddb579cd9403fedf11cb40add640de018f975
David Goodlad <david@goodlad.net>
2014-09-02 07:21:10 -0700

    Use the boxen-downloads s3 mirror
    The official mirror has gone away!

--- files/brews/jenkins.rb
+++ files/brews/jenkins.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Jenkins < Formula
   homepage 'http://jenkins-ci.org'
-  url 'http://mirrors.jenkins-ci.org/war-stable/1.509.3/jenkins.war'
+  url 'https://s3.amazonaws.com/boxen-downloads/jenkins/1.509.3/jenkins.war'
   sha1 'a305d42e62b4daa8f2a6196371dfe389c75a7e22'
   version '1.503-boxen2'
 
