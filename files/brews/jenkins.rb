880179efa409af046f5151e91bd1902d3da72d99
David Goodlad <david@goodlad.net>
2014-09-12 18:13:10 -0700

    It's 1.509.3, not 1.503

--- files/brews/jenkins.rb
+++ files/brews/jenkins.rb
@@ -4,7 +4,7 @@ class Jenkins < Formula
   homepage 'http://jenkins-ci.org'
   url 'https://s3.amazonaws.com/boxen-downloads/jenkins/1.509.3/jenkins.war'
   sha1 'a305d42e62b4daa8f2a6196371dfe389c75a7e22'
-  version '1.503-boxen2'
+  version '1.509.3-boxen1'
 
   skip_clean :all
 
