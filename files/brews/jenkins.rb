a3987d0995c2683c20204cd4eee8618ea49e1280
Mike McQuaid <mike@mikemcquaid.com>
2015-02-18 14:55:18 +0000

    init.pp: fix Jenkins version
    Update the version and set it consistently so it can actually be installed.

--- files/brews/jenkins.rb
+++ files/brews/jenkins.rb
@@ -1,15 +1,15 @@
-require 'formula'
+require "formula"
 
 class Jenkins < Formula
-  homepage 'http://jenkins-ci.org'
-  url 'https://s3.amazonaws.com/boxen-downloads/jenkins/1.565.2/jenkins.war'
-  sha1 'f14b4479236df26b1b4fdd26c2256c296bab1a2e'
-  version '1.565.2-boxen1'
+  homepage "http://jenkins-ci.org"
+  url "http://mirrors.jenkins-ci.org/war-stable/1.580.3/jenkins.war"
+  sha1 "18a8e6ea4a7723b10027ae51ed0196a0d994f622"
+  version "1.580.3-boxen1"
 
   skip_clean :all
 
   def install
     libexec.install "jenkins.war"
+    bin.write_jar_script libexec/"jenkins.war", "jenkins"
   end
-
 end
