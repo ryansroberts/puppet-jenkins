f3e0b816cea91ad920f26914159044121ad0dacf
Ryan Roberts <ryansroberts@gmail.com>
2016-01-21 19:18:27 +0000

    Latest

--- files/brews/jenkins.rb
+++ files/brews/jenkins.rb
@@ -2,9 +2,8 @@ require "formula"
 
 class Jenkins < Formula
   homepage "http://jenkins-ci.org"
-  url "http://mirrors.jenkins-ci.org/war-stable/1.580.3/jenkins.war"
-  sha1 "18a8e6ea4a7723b10027ae51ed0196a0d994f622"
-  version "1.580.3-boxen1"
+  url "https://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war"
+  version "latest-boxen1"
 
   skip_clean :all
 
